f83607b656e22bb53ad9ea62d721e68d1d15fac1
debar <debar@Debarya>
2022-07-13 12:26:07 +0530

    Added customer.java  file

---
+++ BankAccount/src/bank/Customer.java
@@ -0,0 +1,19 @@
+package bank;
+
+public class Customer {
+ int id;
+ int name;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public int getName() {
+	return name;
+}
+public void setName(int name) {
+	this.name = name;
+}
+ 
+}
